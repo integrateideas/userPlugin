a26df7cf77acc2b4387082c32b637ab2428792d4
James <james.kukreja@twinspark.co>
2017-05-17 16:05:21 +0530

    Removed Auth from App controller

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -13,18 +13,6 @@ class AppController extends BaseController
     	$this->loadComponent('RequestHandler');
         $this->loadComponent('Flash');
         $this->loadComponent('Integrateideas/User.Events');
-        $this->loadComponent('Auth', [
-        	'authorize' => 'Controller',
-            'loginAction' => [
-                'controller' => 'Users',
-                'action' => 'login'
-            ],
-            // 'unauthorizedRedirect' => $this->referer() // If unauthorized, return them to page they were just on
-        ]);
-    }
-
-    public function isAuthorized($user){
-    	return true;
     }
 
 }
